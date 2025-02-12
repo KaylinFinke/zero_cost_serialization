2365633524e73499bc8ec669f11ff90f5635ec6c
Kaylin <kaylinfinke@gmail.com>
2025-02-12 10:22:55 -0800

    fix additional safety issues from clang-20
    prefer .begin() to .data() when .begin() is valid.
    prefer std::format to printf
    prefer std::ranges::copy to std::memcpy and std:memmove when we don't need the properties of the C library functions.

--- tools/codegen.cpp
+++ tools/codegen.cpp
@@ -1,7 +1,8 @@
 #include <array>
 #include <concepts>
 #include <cstddef>
-#include <cstdio>
+#include <format>
+#include <iostream>
 #include <span>
 #include <string_view>
 #include <type_traits>
@@ -76,39 +77,39 @@ int main()
 {
 	constexpr auto n = 255;
 
-	puts("#ifndef C17AB45F97AA4A1EAF3129E2BA17DE70");
-	puts("#define C17AB45F97AA4A1EAF3129E2BA17DE70");
-	puts("#ifdef C17AB45F97AA4A1EAF3129E2BA17DE70");
-	puts("");
-	puts("#include <cstddef>");
-	puts("#include <tuple>");
-	puts("#include <type_traits>");
-	puts("");
-	puts("namespace zero_cost_serialization::detail {");
-	puts("\ttemplate <typename, std::size_t, typename = void>\n\tstruct init_n : std::false_type {};");
-	puts("");
+	std::cout << "#ifndef C17AB45F97AA4A1EAF3129E2BA17DE70" << '\n';
+	std::cout << "#define C17AB45F97AA4A1EAF3129E2BA17DE70" << '\n';
+	std::cout << "#ifdef C17AB45F97AA4A1EAF3129E2BA17DE70" << '\n';
+	std::cout << "" << '\n';
+	std::cout << "#include <cstddef>" << '\n';
+	std::cout << "#include <tuple>" << '\n';
+	std::cout << "#include <type_traits>" << '\n';
+	std::cout << "" << '\n';
+	std::cout << "namespace zero_cost_serialization::detail {" << '\n';
+	std::cout << "\ttemplate <typename, std::size_t, typename = void>\n\tstruct init_n : std::false_type {};" << '\n';
+	std::cout << "" << '\n';
 	for (auto i = 0; i not_eq n + 1; ++i)
-		printf("\ttemplate <typename T>\n\tstruct init_n<T, std::size_t{%i}, std::void_t<decltype(T(%.*s))>> : std::true_type {};\n\n", i, i ? i * 3 - 1 : 0, braces<n>);
-	puts("\ttemplate <typename T, typename = void>\n\tstruct make_tuple { constexpr auto operator()(T&) const noexcept\n\t{\n\t\treturn std::make_tuple();\n\t}};\n");
+		std::cout << std::format("\ttemplate <typename T>\n\tstruct init_n<T, std::size_t{{{}}}, std::void_t<decltype(T({:.{}s}))>> : std::true_type {{}};\n\n", i, braces<n>, i ? i * 3 - 1 : 0);
+	std::cout << "\ttemplate <typename T, typename = void>\n\tstruct make_tuple { constexpr auto operator()(T&) const noexcept\n\t{\n\t\treturn std::make_tuple();\n\t}};\n" << '\n';
 	for (auto i = 0, j = digits_n(i) + 1; i not_eq n; j += digits_n(i) + 2)
-		printf("\ttemplate <typename T>\n\tstruct make_tuple<T, std::integral_constant<std::size_t, std::size_t{%i}>> { [[maybe_unused, nodiscard]] constexpr auto operator()(T& t) const noexcept \n\t{\n\t\tauto& [%.*s] = t;\n\t\treturn std::tie(%.*s);\n\t}};\n\n",
-			++i, j, make_binds<n>().data(), j, make_binds<n>().data());
-	puts("\ttemplate <typename T, std::size_t... Is>");
-	puts("\t[[nodiscard]] consteval auto field_count(const std::index_sequence<Is...>&) noexcept");
-	puts("\t{");
-	puts("\t\tauto n = std::size_t{};");
-	puts("\t\t((n = init_n<T, Is>::value ? Is : n), ...);");
-	puts("\t\treturn n;");
-	puts("\t}");
-	puts("");
-	puts("\ttemplate <typename T>");
-	puts("\t[[nodiscard]] consteval auto field_count() noexcept");
-	puts("\t{");
-	printf("\t\treturn field_count<T>(std::make_index_sequence<%d>());\n", n + 1);
-	puts("\t}");
-	puts("}");
-	puts("");
-	puts("#endif");
-	puts("#endif");
+		std::cout << std::format("\ttemplate <typename T>\n\tstruct make_tuple<T, std::integral_constant<std::size_t, std::size_t{{{}}}>> {{ [[maybe_unused, nodiscard]] constexpr auto operator()(T& t) const noexcept \n\t{{\n\t\tauto& [{:.{}s}] = t;\n\t\treturn std::tie({:.{}s});\n\t}}}};\n\n",
+			++i, make_binds<n>().data(), j, make_binds<n>().data(), j);
+	std::cout << "\ttemplate <typename T, std::size_t... Is>" << '\n';
+	std::cout << "\t[[nodiscard]] consteval auto field_count(const std::index_sequence<Is...>&) noexcept" << '\n';
+	std::cout << "\t{" << '\n';
+	std::cout << "\t\tauto n = std::size_t{};" << '\n';
+	std::cout << "\t\t((n = init_n<T, Is>::value ? Is : n), ...);" << '\n';
+	std::cout << "\t\treturn n;" << '\n';
+	std::cout << "\t}" << '\n';
+	std::cout << "" << '\n';
+	std::cout << "\ttemplate <typename T>" << '\n';
+	std::cout << "\t[[nodiscard]] consteval auto field_count() noexcept" << '\n';
+	std::cout << "\t{" << '\n';
+	std::cout << std::format("\t\treturn field_count<T>(std::make_index_sequence<{}>());\n", n + 1);
+	std::cout << "\t}" << '\n';
+	std::cout << "}" << '\n';
+	std::cout << "" << '\n';
+	std::cout << "#endif" << '\n';
+	std::cout << "#endif" << '\n';
 	return 0;
 }
